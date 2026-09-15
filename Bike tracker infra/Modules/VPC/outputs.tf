8596b7d5527fbe29c85334da520d63cc85de3384
Goldee03 <tarundk03@gmail.com>
2026-09-15 09:36:13 +0530

    added some code for instance module

---
+++ Bike tracker infra/Modules/VPC/outputs.tf
@@ -0,0 +1,4 @@
+output "BT_pub_Subnet_id" {
+    value = aws_subnet.BT_pub_Subnet
+  
+}
--- Bike tracker infra/Modules/instance/main.tf
+++ Bike tracker infra/Modules/instance/main.tf
@@ -1,7 +1,26 @@
+resource "tls_private_key" "key_pair" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+  
+}
+
+resource "aws_key_pair" "key" {
+    key_name = var.key_name
+    public_key = tls_private_key.key_pair.public_key_openssh
+  
+}
+
+resource "local_sensitive_file" "instance_key" {
+    filename = "${path.root}/${var.key_name}.pem"
+    content = tls_private_key.key_pair.private_key_pem
+    file_permission = "0400"
+  
+}
+
 resource "aws_instance" "BT_instance" {
-    ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-    instance_type = "t3.micro"
+    ami           = var.ami_id
+    instance_type = var.instance_type
     subnet_id     = module.VPC.BT_pub_Subnet_id
     
-  
+
 }
